ac4c2fdc9229aef5d37722f0ef7a40de1dd262c9
bhushan pawar <pawarbhushan2793@gmail.com>
2021-10-15 08:56:35 +0530

    all css and sdata file

---
+++ src/Sdata.js
@@ -0,0 +1,40 @@
+const Sdata =[
+   {
+    sname:"Dark",
+    imgsrc:"https://theroswellsting.com/wp-content/uploads/2019/09/dark.jpg",
+    title:"Netflix orignal Series",
+    links:"https://www.netflix.com/in/title/80100172",
+   }, 
+
+   {
+    sname:"Extracurricular",
+    imgsrc:"https://static.wikia.nocookie.net/netflix/images/3/3f/Extracurricular_Characters_%284%29.jpg/revision/latest?cb=20200411213211",
+    title:"Netflix orignal Series",
+    links:"https://www.netflix.com/in/title/80990668",
+   }, 
+
+   {
+    sname:"Stranger Things",
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BN2ZmYjg1YmItNWQ4OC00YWM0LWE0ZDktYThjOTZiZjhhN2Q2XkEyXkFqcGdeQXVyNjgxNTQ3Mjk@._V1_.jpg",
+    title:"Netflix orignal Series",
+    links:"https://www.netflix.com/in/title/80057281",
+   }, 
+
+   {
+    sname:"The Vampire Diaries",
+    imgsrc:"https://m.media-amazon.com/images/I/513oZmvN8aL.jpg",
+    title:"Netflix orignal Series",
+    links:"https://www.primevideo.com/detail/0HOK2IOF5E6RFUO8LL6L4LAW3W/_mkw_s-dc&mrntrk=pcrid_452441516049_slid__pgrid_105713381899_pgeo_9304056_x_129940998663_ptid_dsa-932982753595",
+   }, 
+
+   {
+    sname:"Dark",
+    imgsrc:"",
+    title:"Amazon orignal series",
+    links:"",
+   }, 
+
+
+];
+
+export default Sdata;
