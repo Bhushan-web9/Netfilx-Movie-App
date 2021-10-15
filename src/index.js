c89741db8288a78e09a8bc56ca63376d5c732163
bhushan pawar <pawarbhushan2793@gmail.com>
2021-10-15 20:11:31 +0530

    map method apply on sdata file

--- src/index.js
+++ src/index.js
@@ -5,37 +5,30 @@ import "./index.css";
 import Sdata from './Sdata';
 
 
+
+function ncard(val) {
+  return (
+    <Card
+        imgscr={val.imgscr}
+        sname={val.sname}
+        title={val.title}
+        links={val.links}
+    />
+  );
+}
+
+
+
 ReactDOM.render (
   <>
     
     <h1 className="Heading_style">List of top 5 movies</h1>
     
-    
-    <Card
-    
-    imgscr={Sdata[0].imgscr}
-    sname={Sdata[0].sname}
-    title={Sdata[0].title}
-    links={Sdata[0].links}
-    />
+    {Sdata.map(ncard)} 
 
-    <Card
-  
-  imgscr={Sdata[1].imgscr}
-    sname={Sdata[1].sname}
-    title={Sdata[1].title}
-    links={Sdata[1].links}
-    />
-
-    <Card
-   
-   imgscr={Sdata[3].imgscr}
-    sname={Sdata[3].sname}
-    title={Sdata[3].title}
-    links={Sdata[3].links}
-    />
   </>,
-document.getElementById("root")
+
+  document.getElementById("root")
 
 );
 
