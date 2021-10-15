8c24612ed0cf25315fcf6201b3abc47a9f242035
bhushan pawar <pawarbhushan2793@gmail.com>
2021-10-15 10:15:29 +0530

    make movie card in index page

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+
+
+
+
+ReactDOM.render (
+  <>
+    <div className="cards">
+      <div className="card">
+        <img src="https://theroswellsting.com/wp-content/uploads/2019/09/dark.jpg" alt="mypic" className="card_img"/>
+      </div>
+      <div className="card_info">
+        <span className="card_category">A Netflix orignal series</span>
+        <h3 className="card|_title">DARK</h3>
+        <a href="https://www.netflix.com/in/title/80100172" target="_blank">
+          <button>watch now</button>
+        </a>
+      </div>
+    </div>
+  </>,
+document.getElementById("root")
+
+);
+
